cbbf966be3ad7b964814612f94f765ef23fa9cd7
Nguyễn Duy Toản <nguyenduytoanbkdn@gmail.com>
2019-11-22 14:48:03 +0700

    23

---
+++ 23.Speech Synthesis/js/SpeechSynthesis.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const msg = new SpeechSynthesisUtterance();
+    let voices = [];
+    const voicesDropdown = document.querySelector('[name="voice"]');
+    const options = document.querySelectorAll('[type="range"], [name="text"]');
+    const speakButton = document.querySelector('#start');
+    const stopButton = document.querySelector('#stop');
+    msg.text = document.querySelector('[name="text"]').value;
+   
+    function populateVoices() {
+        voices = this.getVoices();
+        voicesDropdown.innerHTML = voices
+                .map( voice => `<option value = "${voice.name}">${voice.name} - ${voice.lang}</option>`)
+                .join('');
+      
+    }
+    function setVoice(){
+        
+        msg.voice = voices.find(voice => voice.name===this.value);
+        
+        toggle();
+    }
+    function toggle( startOver = true){
+        speechSynthesis.cancel();
+        if( startOver = true){
+            speechSynthesis.speak(msg);
+        }
+    }
+    function stopSpeak(){
+        speechSynthesis.cancel();
+    }
+    function setOption(){
+        msg[this.name] = this.value;
+        toggle();
+    }
+
+    speechSynthesis.addEventListener('voiceschanged', populateVoices);
+    voicesDropdown.addEventListener('change',setVoice);
+    options.forEach( option => option.addEventListener('change',setOption));
+    speakButton.addEventListener('click',toggle);
+    stopButton.addEventListener('click',stopSpeak);
+  
+
+}, false)
